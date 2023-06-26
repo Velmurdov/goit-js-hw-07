911a2202090c2c0882db9842107d04dc7c9b9e3f
Velmurdov <batuvalll@gmail.com>
2023-06-26 15:59:21 +0200

    update gallery js1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.js-gallery');
+
+const createGalleryItemEl = ({ preview, original, description }) => 
+`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`; 
+
+galleryEl.innerHTML = galleryItems.map(createGalleryItemEl).join('');
+
+galleryEl.addEventListener('click', (evt) =>{
+    if (evt.target.classList.contains('gallery__image')){ 
+    evt.preventDefault();
+
+    const originalImg = evt.target.dataset.source;
+
+    const instance = basicLightbox.create(`<img src='${originalImg}' width='800' height='600'>`);
+
+    instance.show();
+
+    instance.element().querySelector('img').setAttribute('src', originalImg );
+
+    const closeLightboxEl = (evt) =>{
+        if (evt.key === 'Escape') {
+            instance.close();
+            document.removeEventListener('keydown', closeLightboxEl);
+        }
+    }
+    document.addEventListener('keydown', closeLightboxEl);}
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+import * as basicLightbox from 'basiclightbox'
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`)
+
+instance.show()
+
+
 console.log(galleryItems);
